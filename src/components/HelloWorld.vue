12a76c5de9031a594500d87e90cfd4fdcfa0eba3
Richard Simko <1245031+richardsimko@users.noreply.github.com>
2024-10-25 15:03:57 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+defineProps<{
+  msg: string
+}>()
+
+const elements = ref<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+    <!-- Expected error -->
+    <template v-for="element in elements" :key="element.foo.bar">
+      <p>{{ element.foo.bar }}</p>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
